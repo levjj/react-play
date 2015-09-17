1981edf1627bd7efe3b2d0805c5b0636641adade
Evan Hughes <eshughes@ucsc.edu>
2015-09-16 20:44:08 -0700

    simple todo mvc app

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 /*
 Simple web service to act like a blog
 and do simple CRUD operations.
+Or maybe something simpler.
+
+This is our controller.
 */
 import express from 'express';
+import hbs from 'express-handlebars';
+import bodyParser from 'body-parser';
+import * as todo from './todo.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.engine('hbs', hbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.use(bodyParser());
+
+
+app.get('/', (req, res, next) => {
+  todo.getAllTodo().then( list => {
+    res.render('index', {
+      title: 'Slick Todo List',
+      todo: list
+    });
+  }).catch((err) => {
+    next(err);
+  });
 });
 
-app.post('/create', (req, res) => {
-  //
+app.post('/', (req, res, next) => {
+  todo.addTodoItem(req.body.newtodoitem).then(() => {
+    res.redirect('http://google.com/');
+  }).catch((err) => {
+    next(err);
+  });
+});
+
+app.get('/edit', (req, res) => {
+  res.render('edittodo', {
+    title: 'Edit todo list'
+  });
 });
 app.listen(3000, () => {
   /* eslint no-console:0 */
---
+++ todo.js
@@ -0,0 +1,37 @@
+// This is our model
+
+import fs from 'mz/fs';
+import _ from 'lodash';
+const tdlist = 'todolist';
+
+function ensureListExists() {
+  return fs.exists(tdlist).then((exists) => {
+    if (!exists) {
+      return fs.writeFile(tdlist, '');
+    }
+  });
+}
+
+export function getAllTodo() {
+  return ensureListExists().then(() => {
+    return fs.readFile(tdlist, 'utf-8');
+  }).then(items => {
+    return items.split(/\n/).slice(0, -1);
+  });
+}
+
+export function addTodoItem(item) {
+  return ensureListExists().then(() => {
+    return fs.appendFile(tdlist, item + '\n');
+  });
+}
+
+export function rmTodoItem(index) {
+  return ensureListExists().then(() => {
+    return getAllTodo();
+  }).then(items => {
+    items.splice(index, 1);
+    const itemsWithNL = items.map(i => [i, '\n']);
+    return fs.writeFile(tdlist, _.flatten(itemsWithNL));
+  });
+}
